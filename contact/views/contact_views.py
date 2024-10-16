995cc52537287cf7e0c505f753a8bf8e7c71c2c8
Kelvem01 <kelvemdasilva16@gmail.com>
2024-10-16 17:15:34 -0300

    add new fuction view and 2 new templates

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,10 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from contact.models import Contact
 
 def index(request):
-    contacts = Contact.objects.filter(show=True).order_by('-id')[0:10]
+    contacts = Contact.objects.filter(show=True).order_by('-id')
 
     context = {
-        'contacts': contacts
+        'contacts': contacts,
+        'site_title': 'Contatos - ',
+    }
+    return render(request, 'contact/index.html',context)
+
+def contact(request, contact_id):
+    # Contact.objects.get(pk=contact_id)
+    single_contact = get_list_or_404(
+        Contact.objects.filter(id=contact_id, show=True)
+    )
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
+    context = {
+        'contacts': single_contact,
+        'site_title': site_title,
+    }
+    return render(request, 'contact/contact.html',context)
+
+def search(request):
+    contacts = Contact.objects.filter(show=True).order_by('-id')
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Contatos - ',
     }
     return render(request, 'contact/index.html',context)
